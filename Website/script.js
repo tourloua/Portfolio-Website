edc19f6e8002e0c4cbc39e4c5853cadaf442dd04
tourloua <144152554+tourloua@users.noreply.github.com>
2024-08-28 15:31:18 -0400

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
         if (event.target.tagName === 'TD') {
             const projectName = event.target.innerText;
             switch (projectName) {
-                case 'Google Data Analytics Capstone Project':
+                case 'Google Data Analytics Capstone':
                     window.location.href = 'https://tourloua.github.io/alextourloukisdatacapstone/';
                     break;
                 case 'ENGINEER 1P13 Final Project':
                     window.location.href = 'https://tourloua.github.io/1p13capstone/';
                     break;
-                case 'Solar Tracker Project':
+                case 'Solar Tracker':
                     window.location.href = 'https://tourloua.github.io/Solar-Tracker/';
                     break;
 
